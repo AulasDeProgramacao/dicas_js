a8622a6af9017dbd393134a10fd5e5cb87313c2e
Leonardo Moura Leitão <leonardomleitao@gmail.com>
2016-03-20 08:42:16 -0300

    melhoria de codigo

--- ep04_namespace/variacao_03/adp.helper.js
+++ ep04_namespace/variacao_03/adp.helper.js
@@ -2,6 +2,7 @@ Adp.module.depends('logger');
 
 Adp.helper = {
 	syncVal: function($from, $to) {
+		Adp.logger.debug("Sync Val: " + $from.attr('id') + " => " + $to.attr('id'));
 		$to.val($from.val())
 	}
 };
