173d7f1bcf976c31f261ddb83f58600281812c12
Leonardo Moura Leitão <leonardomleitao@gmail.com>
2016-03-24 18:52:35 -0300

    deixando o código mais enxuto

--- ep04_namespace/variacao_03/adp.js
+++ ep04_namespace/variacao_03/adp.js
@@ -1,7 +1,7 @@
 var Adp = {
 	module: {
 		depends: function(...modules) {
-			var fails = modules.filter(e => {return Adp[e] === undefined});
+			var fails = modules.filter(e => Adp[e] === undefined);
 			if(fails.length) {
 				throw "Os seguintes módulos não foram carregados: " + fails;
 			}
